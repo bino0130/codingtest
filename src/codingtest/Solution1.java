cc82e6229eb722494f43fc8fcf506202834cda8f
bino <ska3287776@naver.com>
2023-04-07 08:56:00 +0900

    Update codingtest

--- src/codingtest/Solution1.java
+++ src/codingtest/Solution1.java
@@ -1,41 +1,72 @@
 package codingtest;
 
 import java.util.*;
-class Solution1 {
-	public int[] solution(int c, int r, int k){
-		int[][] doubleArray = new int[c][r];
+//class Solution1 {
+//	public int[] solution(int c, int r, int k){
+//		int[][] doubleArray = new int[c][r];
 //		for (int[] array : doubleArray) {
 //			
 //		}
-		
-		int[] answer = new int[2];
-		int cnt = 0;
-		int n = doubleArray.length;
-		int[] dx = {0, 1, 0, -1};
-		int[] dy = {1, 0, -1, 0};
-		int d = 1, x = 0, y = 0;
-		while (cnt < k) {
-			cnt++;
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			if(nx < 0 || ny < 0 || nx >= c || ny >= r) {
-				d = (d + 1) % 4;
-				continue;
-			}
-			x = nx;
-			y = ny;
+//		
+//		int[] answer = new int[2];
+//		int cnt = 0;
+//		int n = doubleArray.length;
+//		int[] dx = {0, 1, 0, -1};
+//		int[] dy = {1, 0, -1, 0};
+//		int d = 1, x = 0, y = 0;
+//		while (cnt < k) {
+//			cnt++;
+//			int nx = x + dx[d];
+//			int ny = y + dy[d];
+//			if(nx < 0 || ny < 0 || nx >= c || ny >= r) {
+//				d = (d + 1) % 4;
+//				continue;
+//			}
+//			x = nx;
+//			y = ny;
+//		}
+//		answer[0] = x;
+//		answer[1] = y;
+//		
+//		return answer;
+//	}
+//
+//	public static void main(String[] args){
+//		Solution1 T = new Solution1();
+//		System.out.println(Arrays.toString(T.solution(6, 5, 12)));	
+//		System.out.println(Arrays.toString(T.solution(6, 5, 20)));
+//		System.out.println(Arrays.toString(T.solution(6, 5, 30)));
+//		System.out.println(Arrays.toString(T.solution(6, 5, 31)));
+//	}
+//}
+
+// 1일차 사다리타기
+class Solution1 {
+	public char[] solution(int n, int[][] ladder) { // get 2 parameter
+		char[] answer = new char[n];
+
+		for (int i = 0; i < n; i++) {
+			answer[i] = (char) (i + 65); // answer에 알파벳 출력하는 반복문
 		}
-		answer[0] = x;
-		answer[1] = y;
+
+		for (int[] line : ladder) { // int 배열 line에 ladder 하나씩 넣기
+			for (int x : line) { // int x에 line 하나씩 넣기
+				char tmp = answer[x]; // tmp는 answer의 x번째 인덱스
+				answer[x] = answer[x - 1]; // answer[x]는 answer [x-1]
+				answer[x - 1] = tmp; // answer[x-1]는 tmp
+			}
+		}
 		
 		return answer;
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Solution1 T = new Solution1();
-		System.out.println(Arrays.toString(T.solution(6, 5, 12)));	
-		System.out.println(Arrays.toString(T.solution(6, 5, 20)));
-		System.out.println(Arrays.toString(T.solution(6, 5, 30)));
-		System.out.println(Arrays.toString(T.solution(6, 5, 31)));
+		System.out.println(Arrays.toString(T.solution(5, new int[][] { { 1, 3 }, { 2, 4 }, { 1, 4 } })));
+		System.out.println(Arrays.toString(T.solution(7, new int[][] { { 1, 3, 5 }, { 1, 3, 6 }, { 2, 4 } })));
+		System.out.println(
+				Arrays.toString(T.solution(8, new int[][] { { 1, 5 }, { 2, 4, 7 }, { 1, 5, 7 }, { 2, 5, 7 } })));
+		System.out.println(Arrays.toString(T.solution(12,
+				new int[][] { { 1, 5, 8, 10 }, { 2, 4, 7 }, { 1, 5, 7, 9, 11 }, { 2, 5, 7, 10 }, { 3, 6, 8, 11 } })));
 	}
 }
--- src/codingtest/Solution2.java
+++ src/codingtest/Solution2.java
@@ -4,37 +4,90 @@ import java.util.*;
 
 // 배열 마지막 인덱스 = length - 1
 // 배열에서 peak값들을 먼저 찾아야함
-class Solution2 {
-	public int solution(int[] nums) {
-		int answer = 0;
-		ArrayList<Integer> peaks = new ArrayList<>();
-		
-		
-		for (int i = 1; i < nums.length - 1; i++) {
-			if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
-				peaks.add(i);
-			}
-		}
-		
-		for (int x : peaks) {
-			int left = x;
-			int right = x;
-			int cnt = 1;
-			// left의 값이 0보다 크거나 같고, nums[left - 1]이 nums[left]보다 작다면
-			while (left - 1 >= 0 && nums[left - 1] < nums[left]) {
-				
-			}
-		}
+//
+//class Solution2 {
+//	public int solution(int[] nums) {
+//		int answer = 0;
+//		ArrayList<Integer> peaks = new ArrayList<>();
+//		
+//		for (int i = 1; i < nums.length - 1; i++) {
+//			if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+//				peaks.add(i);
+//			}
+//		}
+//		
+//		for (int x : peaks) {
+//			int left = x;
+//			int right = x;
+//			int cnt = 1;
+//			// left의 값이 0보다 크거나 같고, nums[left - 1]이 nums[left]보다 작다면
+//			while (left - 1 >= 0 && nums[left - 1] < nums[left]) {
+//				
+//			}
+//		}
+//
+//		return answer;
+//	}
+//
+//	public static void main(String[] args) {
+//		Solution2 T = new Solution2();
+//		System.out.println(T.solution(new int[] { 1, 2, 1, 2, 3, 2, 1 }));
+//		System.out.println(T.solution(new int[] { 1, 1, 2, 3, 5, 7, 4, 3, 1, 2 }));
+//		System.out.println(T.solution(new int[] { 3, 2, 1, 3, 2, 4, 6, 7, 3, 1 }));
+//		System.out.println(T.solution(new int[] { 1, 3, 1, 2, 1, 5, 3, 2, 1, 1 }));
+//	}
+//}
 
+class Solution2 {
+	public int[] solution(int[][] board, int k){
+		int[] answer = new int[2];
+		int n = board.length;
+		
+		int[] dx = {0, 1, 0, -1};
+		int[] dy = {1, 0, -1, 0};
+		
+		int x = 0, y = 0, d = 1, cnt = 0;
+		
+		while (cnt < k) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n || board[nx][ny] == 1) {
+				d = (d + 1) % 4;
+				cnt++;
+				continue;
+			}
+			
+			x = nx;
+			y = ny;
+			answer[0] = x;
+			answer[1] = y;
+			cnt++;
+		}
 		
 		return answer;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Solution2 T = new Solution2();
-		System.out.println(T.solution(new int[] { 1, 2, 1, 2, 3, 2, 1 }));
-		System.out.println(T.solution(new int[] { 1, 1, 2, 3, 5, 7, 4, 3, 1, 2 }));
-		System.out.println(T.solution(new int[] { 3, 2, 1, 3, 2, 4, 6, 7, 3, 1 }));
-		System.out.println(T.solution(new int[] { 1, 3, 1, 2, 1, 5, 3, 2, 1, 1 }));
+		int[][] arr1 = {{0, 0, 0, 0, 0}, 
+			{0, 1, 1, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr1, 10)));
+		int[][] arr2 = {{0, 0, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0, 0}, 
+			{0, 0, 0, 0, 0, 1}, 
+			{1, 1, 0, 0, 1, 0}, 
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr2, 20)));
+		int[][] arr3 = {{0, 0, 1, 0, 0}, 
+			{0, 1, 0, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 0, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr3, 25)));
 	}
 }
